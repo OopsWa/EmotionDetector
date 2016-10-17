5efbd5ea8d1580fe4291ef7c37dbec83acd58ece
OopsWa <1002901669@qq.com>
2016-10-17 19:01:22 +0900

    登录注册

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(session({
   saveUninitialized: true
 }));
 app.use('/', routes);
-app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
